1ffab1cb32daa938ddcb29000525e37216d88120
missmbuvi04 <m.mbuvi@alustudent.com>
2024-03-22 08:47:02 -0700

    programming

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-This script fetches the status from a given URL using the urllib package.
-It then displays the body of the response with required formatting.
+Fetches status from either https://intranet.hbtn.io/status
+or http://0.0.0.0:5050/status using urllib.
 """
 
 import urllib.request
 import urllib.error
 
+
 def fetch_status(url):
     """
-    Fetches and prints the status from the given URL
+    Fetches and prints the status from the given URL.
+
     Args:
-        url (str): The URL to fetch the status from
+        url (str): The URL to fetch the status from.
     """
     try:
         with urllib.request.urlopen(url) as response:
@@ -22,6 +24,7 @@ def fetch_status(url):
             print("\t- utf8 content:", body.decode('utf-8'))
     except urllib.error.URLError as e:
         print("Error:", e.reason)
+
 
 if __name__ == "__main__":
     fetch_status("https://intranet.hbtn.io/status")
