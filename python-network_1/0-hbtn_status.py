910087bff5292f68d3f3b22ce92b383d8ebb875b
missmbuvi04 <m.mbuvi@alustudent.com>
2024-03-22 08:35:14 -0700

    programming

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-# python script that fetches 'https://alu-intranet.hbtn.io/status'
-"""
-    fetch 'https://intranet.hbtn.io/status'
-"""
-import urllib.request
+# python script that fetches 'https://alu-intranet.hbtn.io/status
 
+import urllib.request
+import urllib.error
+
+def fetch_status(url):
+    """
+    Fetches and prints the status from the given URL
+    """
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print("Body response:")
+            print("\t- type:", type(body))
+            print("\t- content:", body)
+    except urllib.error.URLError as e:
+        print("Error:", e.reason)
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    fetch_status("https://alu-intranet.hbtn.io/status")
