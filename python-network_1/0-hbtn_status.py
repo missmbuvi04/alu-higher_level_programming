12716041b635fce3ff366f26b899750883db078c
missmbuvi04 <m.mbuvi@alustudent.com>
2024-03-22 09:14:59 -0700

    programming

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,27 +5,13 @@ or http://0.0.0.0:5050/status using urllib.
 """
 
 import urllib.request
-import urllib.error
-
-
-def fetch_status(url):
-    """
-    Fetches and prints the status from the given URL.
-
-    Args:
-        url (str): The URL to fetch the status from.
-    """
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type:", type(body))
-            print("\t- content:", body)
-            print("\t- utf8 content:", body.decode('utf-8'))
-    except urllib.error.URLError as e:
-        print("Error:", e.reason)
 
 
 if __name__ == "__main__":
-    fetch_status("https://intranet.hbtn.io/status")
-
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
