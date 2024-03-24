8eb0f76909649803a697665d487841100f079d3b
missmbuvi04 <m.mbuvi@alustudent.com>
2024-03-24 12:26:30 -0700

    program

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
 """
-Fetches https://alu-intranet.hbtn.io/status and prints the response.
-"""
+
 import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
-    # Create a request object
-    with urllib.request.urlopen(url) as response:
-        # Read the response content
-        content = response.read()
-
-        # Decode the content to UTF-8
-        utf8_content = content.decode('utf-8')
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
