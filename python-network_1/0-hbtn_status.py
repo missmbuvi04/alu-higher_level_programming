2d65c355ba5a3b24434fd059e8d0c551ca5f35c6
missmbuvi04 <m.mbuvi@alustudent.com>
2024-03-24 11:35:20 -0700

    programming

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,64 +1,22 @@
 #!/usr/bin/python3
 
-<<<<<<< HEAD
-"""A script that
-
-- fetches https://alx-intranet.hbtn.io/status.
-
-- uses urlib package
-
-"""
-
-
-
-
-
-if __name__ == '__main__':
-    
-    import urllib.request
-    
-
-    
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        
-        content = res.read()
-        
-        print("Body response:")
-        
-        print("\t- type: {}".format(type(content)))
-=======
-"""Python script that fetches"""
+"""Script that fetches status from a URL using urllib."""
 
 import urllib.request
 
-
-
-
-
 def getStatus():
-    
-    """a"""
-    
+    """
+    Fetches the status from 'https://intranet.hbtn.io/status'
+    and prints out the response body.
+    """
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        
         content = res.read()
-        
         type_content = type(content)
-        
         print("Body response:")
-        
         print("\t- type: {}".format(type_content))
->>>>>>> a1cb096b6652ca13a4e5fb24e043d4fbe759e625
-        
         print("\t- content: {}".format(content))
-        
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
-<<<<<<< HEAD
-=======
-        
 
-        
 if __name__ == "__main__":
-    
     getStatus()
->>>>>>> a1cb096b6652ca13a4e5fb24e043
+
