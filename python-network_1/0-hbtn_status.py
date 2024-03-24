088b961c86a0312abf76c0480451e49525b242c6
missmbuvi04 <m.mbuvi@alustudent.com>
2024-03-24 11:25:10 -0700

    programming

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+- fetches http://0.0.0.0:5050/status.
+- uses urllib package
 """
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    url = 'http://0.0.0.0:5050/status'
+
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
