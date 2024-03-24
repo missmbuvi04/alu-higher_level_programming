36fe9ea1f042c6e9eda6d7140040f5450941af73
missmbuvi04 <m.mbuvi@alustudent.com>
2024-03-24 12:20:21 -0700

    program

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,54 +1,12 @@
 #!/usr/bin/python3
-"""Documented by Lsblack"""
-
 """ module uses the request module
     to send a request to server """
-    <<<<<<< HEAD
-"""
-
-requests model
-
-"""
-
-
-
-if __name__ == '__main__':
-    
-    import requests
-    
-    html = requests.get('https://alx-intranet.hbtn.io/status')
-    
-    print("Body response:")
-    
-    print("\t- type: {}".format(html.text.__class__))
-    
-    print("\t- content: {}".format(html.text))
-=======
-"""Takes in a URL, sends a request to the URL and"""
-
 import requests
 
 
-
-
-
-def getStatus():
-    
-    req = requests.get('https://intranet.hbtn.io/status')
-    
-    content = req.text
-    
-
-    
-    print("Body response:")
-    
-    print("\t- type: {}".format(type(content)))
-    
-    print("\t- content: {}".format(content))
-    
-
-    
 if __name__ == "__main__":
-    
-    getStatus()
->>>>>>> a1cb096b6652ca13a4e5fb24e043d4fbe759e625
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    data = r.text
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
